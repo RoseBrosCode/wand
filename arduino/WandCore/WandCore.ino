4756ea69ee99939eb2b513e061a67fa55047d768
Ryan Rose <rytrose@gmail.com>
2021-02-10 21:28:47 -0500

    update pins

--- arduino/WandCore/WandCore.ino
+++ arduino/WandCore/WandCore.ino
@@ -83,7 +83,7 @@ void setup()
   Serial.println(WiFi.localIP());
 
   // Configure color sensor
-  setupColorSensor(16);
+  setupColorSensor(14);
 
   // Turn onboard LED off
   setColorSensorLED(false);
@@ -95,7 +95,7 @@ void setup()
   setupGesturePrediction();
 
   // Configure RGB LED
-  setupRGBLED(32, 15, 33);
+  setupRGBLED(21, 17, 16);
 
   // Turn RGB LED off
   setRGBLED(0, 0, 0);
@@ -111,6 +111,27 @@ void setup()
   digitalWrite(LED_BUILTIN, HIGH);
   delay(200);
   digitalWrite(LED_BUILTIN, LOW);
+}
+
+void testDevices()
+{
+  // Test RGB LED
+  setRGBLED(255, 0, 0);
+  delay(1000);
+  setRGBLED(0, 255, 0);
+  delay(1000);
+  setRGBLED(0, 0, 255);
+  delay(1000);
+
+  // Test color sensor
+  setColorSensorLED(true);
+  delay(1000);
+  setColorSensorLED(false);
+  for (int i = 0; i < 5; i++)
+  {
+    getRGB(&sensorR, &sensorG, &sensorB);
+    delay(200);
+  }
 }
 
 void loop() 
