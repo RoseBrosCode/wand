5648cda4182c76db48b313b4144ad71b7383ffd1
Zach Rose <zwrose@gmail.com>
2021-05-02 11:34:48 -0400

    add remote logging and wifi to DataCollection

--- arduino/DataCollection/DataCollection.ino
+++ arduino/DataCollection/DataCollection.ino
@@ -1,3 +1,37 @@
+#define HOST_NAME "sparklewand"
+////////////////////////////////////////////////////////////////////////////////////
+// WiFi Client
+////////////////////////////////////////////////////////////////////////////////////
+#include <WiFiClient.h>
+#include <WiFi.h>
+#include "environ.h"
+
+// Connects to WiFi
+WiFiClient client;
+////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////
+// mDNS
+////////////////////////////////////////////////////////////////////////////////////
+#include <DNSServer.h>
+#include <ESPmDNS.h>
+////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////
+// OTA updating
+////////////////////////////////////////////////////////////////////////////////////
+#include <ArduinoOTA.h>
+int otaProgress = 0;
+////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////
+// Set up remote debug
+////////////////////////////////////////////////////////////////////////////////////
+#include <RemoteDebug.h>        //https://github.com/JoaoLopesF/RemoteDebug
+
+RemoteDebug Debug;
+////////////////////////////////////////////////////////////////////////////////////
+
 // Add Arduino library for VSCode IntelliSense
 #include <Arduino.h>
 
@@ -15,6 +49,65 @@ void setup()
     while (!Serial)
     {
     };
+
+    // Connect to WiFi
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(mySSID);
+    WiFi.begin(mySSID, myPASS);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  
+  
+    // Set Hostname (for OTA and Debug)
+    ArduinoOTA.setHostname(HOST_NAME);
+  
+    // Configure Remote Debug
+    MDNS.addService("telnet", "tcp", 23);
+    Debug.begin(HOST_NAME); // Initialize the WiFi server
+    Debug.setResetCmdEnabled(true); // Enable the reset command
+    Debug.showProfiler(true); // Profiler (Good to measure times, to optimize codes)
+    Debug.showColors(true); // Colors 
+    Debug.setSerialEnabled(true); // Forwards all logs to serial
+  
+    // Configure OTA
+    ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        debugI("Start updating %s", type);
+      })
+      .onEnd([]() {
+        debugI("OTA upload complete. Rebooting...");
+      })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        int numProgress = 100 * progress;                       // this step enables the next step
+        int percent = numProgress / total;                      // get progress as a percentage - int will be truncated
+        if (percent % 10 == 0 && percent > otaProgress) {       // every 10 percent of progress, print the percentage, and don't print the same percentage repeatedly
+          otaProgress = percent;                                // allows for only printing the percentage once, since there will be numerous calls where percent % 10 results in the same number
+          debugI("OTA upload is %d percent complete", percent);
+        }
+      })
+      .onError([](ota_error_t error) {
+        debugE("OTA Error[%s]: ", error);
+        if (error == OTA_AUTH_ERROR) debugE("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) debugE("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) debugE("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) debugE("Receive Failed");
+        else if (error == OTA_END_ERROR) debugE("End Failed");
+      });
+  
+    ArduinoOTA.begin();
 
     // Initialize I2C
     Wire.begin();
@@ -68,6 +161,12 @@ void loop()
     // Wait for acceleration to begin gesture sample collection
     while (samplesRead == numSamplesPerGesture)
     {
+        // Check for OTA
+        ArduinoOTA.handle();
+    
+        // facilitates remote debugging
+        Debug.handle();
+        
         if (myICM.dataReady()) 
         {
             // Read the sensor
@@ -111,33 +210,42 @@ void loop()
             aX = myICM.accX() / 1000;
             aY = myICM.accY() / 1000;
             aZ = myICM.accZ() / 1000;
+//            aX = round(myICM.accX()) / 1000;
+//            aY = round(myICM.accY()) / 1000;
+//            aZ = round(myICM.accZ()) / 1000;
 
             // Read gyroscope values
             gX = myICM.gyrX();
             gY = myICM.gyrY();
             gZ = myICM.gyrZ();
+//            gX = round(myICM.gyrX() * 1000) / 1000;
+//            gY = round(myICM.gyrY() * 1000) / 1000;
+//            gZ = round(myICM.gyrZ() * 1000) / 1000;
 
             // Increase sample counter
             samplesRead++;
 
             // Print the data in expected CSV format
-            Serial.print(aX, 3);
-            Serial.print(',');
-            Serial.print(aY, 3);
-            Serial.print(',');
-            Serial.print(aZ, 3);
-            Serial.print(',');
-            Serial.print(gX, 3);
-            Serial.print(',');
-            Serial.print(gY, 3);
-            Serial.print(',');
-            Serial.print(gZ, 3);
-            Serial.println();
+//            Serial.print(aX, 3);
+//            Serial.print(',');
+//            Serial.print(aY, 3);
+//            Serial.print(',');
+//            Serial.print(aZ, 3);
+//            Serial.print(',');
+//            Serial.print(gX, 3);
+//            Serial.print(',');
+//            Serial.print(gY, 3);
+//            Serial.print(',');
+//            Serial.print(gZ, 3);
+//            Serial.println();
+          
+            debugI(",%f,%f,%f,%f,%f,%f", aX, aY, aZ, gX, gY, gZ);
 
             // Add an empty line after the last sample
             if (samplesRead == numSamplesPerGesture)
             {
-                Serial.println();
+//                Serial.println();
+                debugI("");
             }
         }
     }
@@ -154,4 +262,6 @@ void loop()
     digitalWrite(LED_BUILTIN, HIGH);
     delay(200);
     digitalWrite(LED_BUILTIN, LOW);
+
+    
 }
